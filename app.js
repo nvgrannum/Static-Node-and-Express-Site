6a7f078fa73884551624793ad030ced510f61b8d
nvgrannum <79998374+nvgrannum@users.noreply.github.com>
2021-04-26 18:30:05 -0700

    Updates
    Moved images folder inside view folder. updated dynamic pages to correct id

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 //const https = require('https');
 const { render } = require('pug');
-const {data} = require('./data.json');
+const {projects} = require('./data.json');
 const app = express();
 const router = express.Router();
 
@@ -9,8 +9,9 @@ app.set('view engine', 'pug');
 app.use(express.static('public'));
 
 app.get('/', (req, res)=> {
-    //text
-    res.render('index.pug');
+    const {id} = req.params;
+    const project = projects[id];
+    res.render('index.pug', {id, projects, project});
 });
 
 app.get('/about', (req, res) => {
@@ -19,8 +20,23 @@ app.get('/about', (req, res) => {
 
 app.get('/project/:id', (req, res) => {
     const {id} = req.params;
-    const {projects} = data
-    res.render('project', {id, projects})
+    const project = projects[id];
+    res.render('project', {project})
 });
 
+app.use((err, req,res,next)=>{
+    res.locals.error= err;
+
+    if (err.status === 404) {
+        return res.render('page-not-found', {err});
+    } else if (err.status === 500) {
+        return res.render('error', {err});
+    }
+
+    next(err)
+});
+
+
+
+module.exports = router;
 app.listen(3000); 
