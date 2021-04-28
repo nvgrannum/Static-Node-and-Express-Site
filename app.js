63d316f28ccd7c7a1f4747aa8cb0ad5d00bbe970
nvgrannum <79998374+nvgrannum@users.noreply.github.com>
2021-04-27 17:53:18 -0700

    updates

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
 const express = require('express');
-//const https = require('https');
 const { render } = require('pug');
 const {projects} = require('./data.json');
 const app = express();
@@ -11,34 +10,54 @@ app.use('/static',express.static('public'));
 app.get('/', (req, res)=> {
     const {id} = req.params;
     const project = projects[id];
-    res.render('index.pug', {id, projects, project});
+    res.render('index.pug', { projects, project});
 });
 
 app.get('/about', (req, res) => {
     res.render('about')
 });
 
+//feeds each project from the data.json file to an individual project page
 app.get('/project/:id', (req, res) => {
     const {id} = req.params;
     const project = projects[id];
     res.render('project', {project})
 });
 
+//redirects the user to the homepage if the project number gets deleted since homepage has list of all projects.
+app.get('/project', (req,res) =>{
+    res.redirect('/');
+})
+
+app.get('/error', (req,res)=>{
+    const err = new Error('500s')
+    err.status=500;
+    next(err);
+})
+
+app.use((req,res,next)=>{
+    const err = new Error("Page not found :(");
+    err.status=404;
+    next(err)   
+});
+
 app.use((err, req,res,next)=>{
     res.locals.error= err;
 
-    if (res.status(404)) {
-        err.status=404;
+    if (err.status ===404) {
+        res.status(err.status);
         err.message="That page does not exist :("
         return res.render('page-not-found', {err});
     } else {
-        err.status=500;
+        err.status = 500;
+        res.status(err.status);
         err.message="Server made an oops :( Try again"
         return res.render('error', {err});
     }
 
     next(err)
 });
+
 
 
 module.exports = router;
